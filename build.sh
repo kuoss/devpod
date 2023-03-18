26188e7ef52f6c9e92664bc13db6351c6b33b831
jmnote <opcore@gmail.com>
2023-03-18 04:07:39 +0000

    .

--- build.sh
+++ build.sh
@@ -11,8 +11,8 @@ FROM golang:1.19-bullseye
 
 RUN curl -sL https://deb.nodesource.com/setup_18.x | bash -
 RUN curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+&& chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 RUN set -eux; \
 	\
 	apt-get update; \
