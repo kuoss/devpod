84325b5626298fb2e8838d789596999ffb34ccd8
jmnote <opcore@gmail.com>
2023-03-18 07:52:41 +0000

    .

--- k8s/files/init.sh
+++ k8s/files/init.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 cat <<EOF > /root/.bashrc
 alias ls='ls --color'
-alias ll='ls --color -l'
+alias ll='ls --color -alF'
 alias l='ls --color -lA'
 
-
 echo "https://$GIT_USER_NAME:$GITHUB_TOKEN@github.com" > ~/.git-credentials
-git config --global credential.helper 'store ~/.git-credentials'
+git config --global credential.helper 'store --file ~/.git-credentials'
 code /ws
 EOF
 
-mkdir ~/.vscode-server 
-ln -s ~/.vscode-server /ws/.vscode-server
+mkdir /ws/.vscode-server
+ln -s /ws/.vscode-server ~/.vscode-server
 
-git config --global user.name  $GIT_USER_NAME
-git config --global user.email $GIT_USER_EMAIL
+git config --global user.name   $GIT_USER_NAME
+git config --global user.email  $GIT_USER_EMAIL
 git config --global pull.rebase false
+
+[ ! -d devpod ] \
+&& git clone https://github.com/kuoss/devpod.git \
+&& ln -rs devpod/Makefile.txt ./Makefile
 
 exec sleep infinity
