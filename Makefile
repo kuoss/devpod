9dcd2da7e839554e412cdf6ae444cbd5ff0cc0ba
jmnote <opcore@gmail.com>
2023-03-18 06:56:18 +0000

    .

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 build:
 	cd docker && make build
 
+pvc:
+	cd k8s && kubectl apply -f _pvc.yaml
+
 deploy:
 	cd k8s && kubectl apply -k .
--- k8s/files/init.sh
+++ k8s/files/init.sh
@@ -8,8 +8,10 @@ echo $GH_TOKEN | GH_TOKEN= gh auth login --with-token
 code /ws
 EOF
 
+ln -s /root/.vscode-server /ws/.vscode-server
+
 git config --global user.name  $GIT_USER_NAME
 git config --global user.email $GIT_USER_EMAIL
 git config --global pull.rebase false
 
-sleep infinity
+exec sleep infinity
